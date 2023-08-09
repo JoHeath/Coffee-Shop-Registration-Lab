7d710a23bb24730ccd4d4174ccb9950067af9691
joseph heath <heathj873@gmail.com>
2023-08-09 16:08:25 -0400

    first

---
+++ Coffee Shop Registration Lab/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Coffee_Shop_Registration_Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_Shop_Registration_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Joe's Coffee Shop";
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult ThankYou(User s)
+        {
+            return View(s);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Coffee Shop Registration Lab/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Coffee_Shop_Registration_Lab.Models
+{
+    public class User
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+        public bool privacy  { get; set; }
+    }
+}
